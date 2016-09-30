cebcc303a969df8ee0c04b612cca3ce9fbcedfc3
my <my@my-PC>
2016-09-30 23:22:48 +0800

    her 修改 "her"

--- src/Lethergo_d0441381.java
+++ src/Lethergo_d0441381.java
@@ -3,10 +3,10 @@ public class Lethergo_d0441381 {
 
 	public static void main(String[] args) {
 		String lyrics="Let it go!Let it go!Cannot hold it back anymore";
-		System.out.println("原歌詞"+lyrics);
-		lyrics=lyrics.replaceAll("it","her");
+		System.out.println("原歌詞:"+lyrics);
+		lyrics=lyrics.replaceAll("it","\"her\"");
 		lyrics=lyrics.replaceAll("Cannot","Can't");
-		System.out.println("更改過後"+lyrics);
+		System.out.println("更改過後:"+lyrics);
 
 	}
 
