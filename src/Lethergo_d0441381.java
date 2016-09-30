8f592a2f51b1ee785ea9e79ac906ef9259445217
my <my@my-PC>
2016-09-30 16:58:46 +0800

    做 it 修改成her以及 Cannot 改成 Can't

---
+++ src/Lethergo_d0441381.java
@@ -0,0 +1,13 @@
+
+public class Lethergo_d0441381 {
+
+	public static void main(String[] args) {
+		String lyrics="Let it go!Let it go!Cannot hold it back anymore";
+		System.out.println("原歌詞"+lyrics);
+		lyrics=lyrics.replaceAll("it","her");
+		lyrics=lyrics.replaceAll("Cannot","Can't");
+		System.out.println("更改過後"+lyrics);
+
+	}
+
+}
